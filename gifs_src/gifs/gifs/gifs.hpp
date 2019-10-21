b5ffe21750492f6ecd2ac56d91088ab5102a5ef0
Maximilian Menger <menger.maximilian@gmail.com>
2019-10-21 10:42:35 +0200

    local changes

--- gifs_src/gifs/gifs/gifs.hpp
+++ gifs_src/gifs/gifs/gifs.hpp
@@ -4,15 +4,10 @@
 #include <string>
 #include <memory>
 
-#include "gifs/sh/sh.hpp"
-#include "gifs/es/es.hpp"
-
 namespace gifs 
 {
-/* Virtual base class, defining all operations that 
- * should be called from the MD software
- *
- * P
+/* Base class, defining all operations that 
+ * should be called from the MD software/nuclear propagator
  */
 class Gifs
 {
@@ -23,13 +18,10 @@ public:
     virtual void do_surface_hopping()  = 0;
     // compute functions
     // mechanical embedding:
-    virtual unit get_energy(std::vector<double>& crd) = 0;
-    virtual unit get_eandg(std::vector<double>& crd, std::vector<unit>& grad) = 0;
+    inline double get_energy(std::vector<double>& crd) = 0;
+    inline double get_eandg(std::vector<double>& crd, std::vector<unit>& grad) = 0;
 private:
     std::string _configfile{};
-    // Implementation
-    std::unique_prt<SurfaceHopping> _sh{nullptr};
-    std::unique_prt<ElectronicStructure> _es{nullptr};
 };
 
 
