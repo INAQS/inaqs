5d2281ef6f140aa0d67d96b941027f6a1c47b2cd
Vale Cofer-Shabica <valecs@sas.upenn.edu>
2019-12-18 11:32:00 -0500

    removing test cruft from qm

--- gifs_src/gifs/dynamics.cpp
+++ gifs_src/gifs/dynamics.cpp
@@ -1,1 +1,19 @@
 #include "dynamics.hpp"
+
+namespace BOMD{
+
+  BOMD(std::string config){
+  }
+
+  template<typename T>
+  T get_gradient(T* qm_crd, T* mm_crd,
+		 T* mm_chg, T* qm_gradient,
+		 T* mm_gradient){
+    static_assert<std::is_floating_point<T>::value, "Error Msg">;
+  }
+
+  /*No velocity resacle for BOMD*/
+  template<typename T> T rescale_velocities(T* total_gradient, T* masses, T* velocities){}
+
+}
+
--- gifs_src/gifs/qm.cpp
+++ gifs_src/gifs/qm.cpp
@@ -11,21 +11,6 @@
 
 #define FMT "%12.8g"
 
-void p3vec(std::vector<double> vec){
-  if (0 != vec.size()%3){
-    throw std::invalid_argument("Vector not in R3!");
-  }
-
-  double x,y,z;
-  
-  for (size_t i = 0; i < vec.size()/3; i++){
-    x = vec[i*3 + 0];
-    y = vec[i*3 + 1];
-    z = vec[i*3 + 2];
-    printf(FMT "\t" FMT "\t" FMT "\n", x, y, z);
-  }
-}
-
 QMInterface::QMInterface(size_t nqm, std::vector<int> &qmid){
   NQM = nqm;
   atomids = qmid;
@@ -208,43 +193,3 @@ $rem
   ifile.close();
 }
 
-/*
-int main(void){
-  std::vector<double> qm_crd, mm_crd, mm_chg;
-  size_t nqm = 4;
-  size_t nmm = 2;
-
-  std::vector<int> qmids = {9, 9, 9, 9};
-  
-  QMInterface* qm = new QMInterface(nqm, qmids);
-  
-  qm_crd = { 2.1194186904,    -0.1478987347,     0.0000000000,
-	     1.4498754181,     1.0202212338,     0.0000000000,
-	     -1.4498754181,   -1.0202212338,     0.0000000000,
-	     -2.1194186904,    0.1478987347,     0.0000000000};
-  
-  mm_crd = {0.00000, 0.00000,  1.00000,
-	    0.00000, 0.00000, -1.00000};
-
-  mm_chg = {0.1, -0.1};
-  
-  std::cout << "qm_crd" << std::endl; p3vec(qm_crd);
-  std::cout << "mm_crd" << std::endl; p3vec(mm_crd);
-
-  qm->update(qm_crd, mm_crd, mm_chg);
-  
-  std::vector<double> g_qm, g_mm;
-  g_qm.resize((size_t) 3 * nqm);
-  g_mm.resize((size_t) 3 * nmm);
-  
-  PropMap props;
-  props.emplace(QMProperty::qmgradient, &g_qm);
-  props.emplace(QMProperty::mmgradient, &g_mm);
-  qm->get_properties(props);
-  
-  std::cout << "g_qm" << std::endl; p3vec(g_qm);
-  std::cout << "g_mm" << std::endl; p3vec(g_mm);
-
-  return 0;
-}
-*/
