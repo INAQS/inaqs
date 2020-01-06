3a3b7619393e15065a3ab425c45816dc3f612590
Vale Cofer-Shabica <valecs@sas.upenn.edu>
2020-01-06 18:33:33 -0500

    fixed up nve/nvt examples; they run!

--- examples/glycine/nvt/run.sh
+++ examples/glycine/nvt/run.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 readonly NAME=gly_nvt
 
-grompp -f nvt.mdp -c glycine.gro -p ../glycine.top -n ../gly.ndx -o ${NAME}.tpr
+grompp -f nvt.mdp -c nvt.gro -p ../glycine.top -n ../gly.ndx -o ${NAME}.tpr
 mdrun -v -deffnm ${NAME}
 
