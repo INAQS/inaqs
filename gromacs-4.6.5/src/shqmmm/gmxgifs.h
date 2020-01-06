40e20c047c55458cc99a00a439b2efb21b2d3bb5
Vale Cofer-Shabica <valecs@sas.upenn.edu>
2020-01-06 18:32:11 -0500

    include compilier error note in gmxgifs.h

--- gromacs-4.6.5/src/shqmmm/gmxgifs.h
+++ gromacs-4.6.5/src/shqmmm/gmxgifs.h
@@ -6,4 +6,26 @@ void gifs_scale_velocities(float* v, float* f, float* invmass);
 float gifs_do_qm_forces(size_t nqm, const int* qm_atomids, const float* qm_crd,
 			size_t nmm, const float* mm_crd, const float* mm_chg,
                        float* f, float* fshift);
+
+/*
+
+FIXME: Compilation errors to be dealt with.
+
+/data/home/vale/projects/GQSH/gifs/gromacs-4.6.5/src/kernel/md.c: In function ‘do_md’:
+/data/home/vale/projects/GQSH/gifs/gromacs-4.6.5/src/kernel/md.c:1246:35: warning: passing argument 1 of ‘gifs_scale_velocities’ from incompatible pointer type [-Wincompatible-pointer-types]
+             gifs_scale_velocities(state->v, f, mdatoms->invmass);
+                                   ^~~~~
+In file included from /data/home/vale/projects/GQSH/gifs/gromacs-4.6.5/src/kernel/md.c:108:0:
+/data/home/vale/projects/GQSH/gifs/gromacs-4.6.5/src/kernel/../shqmmm/gmxgifs.h:5:6: note: expected ‘float *’ but argument is of type ‘real (*)[3] {aka float (*)[3]}’
+ void gifs_scale_velocities(float* v, float* f, float* invmass);
+      ^~~~~~~~~~~~~~~~~~~~~
+/data/home/vale/projects/GQSH/gifs/gromacs-4.6.5/src/kernel/md.c:1246:45: warning: passing argument 2 of ‘gifs_scale_velocities’ from incompatible pointer type [-Wincompatible-pointer-types]
+             gifs_scale_velocities(state->v, f, mdatoms->invmass);
+                                             ^
+In file included from /data/home/vale/projects/GQSH/gifs/gromacs-4.6.5/src/kernel/md.c:108:0:
+/data/home/vale/projects/GQSH/gifs/gromacs-4.6.5/src/kernel/../shqmmm/gmxgifs.h:5:6: note: expected ‘float *’ but argument is of type ‘real (*)[3] {aka float (*)[3]}’
+ void gifs_scale_velocities(float* v, float* f, float* invmass);
+
+*/
+
 #endif
