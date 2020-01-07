5a584501b2bb87b07432def1a5a763f2dde1e37e
Vale Cofer-Shabica <valecs@sas.upenn.edu>
2020-01-07 13:55:12 -0500

    set-up HBDI example

--- examples/hbdi_water/run.sh
+++ examples/hbdi_water/run.sh
@@ -6,18 +6,15 @@ export GMX_MAXBACKUP=-1
 
 readonly NAME=HBDI_water
 
-readonly structurein=conf.gro
-readonly structure=conf-centered.gro
+readonly structure=conf.gro
 readonly topology=qmmm.top
 readonly index=index.ndx
+readonly grompp=grompp-slim.mdp
 # The index file defines the following groups:
 ## System, �7, HBD, XXX, NA, Water, SOL, non-Water, Ion, HBD, XXX, NA, SOL_ions, HBD_XXX, System_&_!SOL_ions, a_1-8, QMatoms, System_&_!QMatoms, MMatoms
 
-# center the molecule in a large box
-editconf -f ${structurein} -o ${structure} -c
-
 # process all the files into something we can run with
-#grompp -f qmmm.mdp -c qmmm-solvated.gro -p glycine.top -n ${NAME}.ndx -o ${NAME}.tpr
+grompp -f ${grompp} -c ${structure} -p ${topology} -n ${index} -o ${NAME}.tpr
 
 # do the run; must specify -nt 1
 #mdrun -nt 1 -v -deffnm ${NAME}
