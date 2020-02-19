933ec1e29aa33d6063e6ef3cdb4aa41b1f6ca58b
Maximilian Menger <menger.maximilian@gmail.com>
2020-02-19 14:11:11 +0100

    added conversion class

---
+++ gifs_src/gifs/conversion.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <armadillo>
+
+#include "conversion.hpp"
+
+static 
+Conversion 
+Conversion::from_elementary(double mass, double length, double time) {
+    const double internal_crd_length = 1e-10;          // m, Angstrom
+    const double internal_length = 5.29177210903e-11;  // m, Bohr
+    const double internal_mass =  9.1093837015e-31;    // kg, au
+    const double internal_time =  2.418884326509e-17;  // seconds, au
+    // const double internal_energy_ex = 1.602176487e-19;    // Joule, eV
+    const double internal_velocity =  internal_length/internal_time;  // m/s
+    const double internal_energy = internal_mass * internal_length * internal_length /
+                                    (internal_time * internal_time);
+    const double internal_force = internal_energy/internal_length;
+    //
+    const double energy = mass * length * length/(time * time);
+    const double force = energy/length;
+    const double velocity = length/time;
+    //
+    return Conversion(internal_energy/energy, length/internal_crd_length, internal_force/force, velocity/internal_velocity);
+};
---
+++ include/conversion.hpp
@@ -0,0 +1,50 @@
+#ifndef GIFS_CONVERSION_H
+#define GIFS_CONVERSION_H
+
+#include <algorithm>
+#include <armadillo>
+
+
+/* Direct conversion! */
+class Conversion
+{
+public:
+    Conversion(double in_en_from, double in_coords_to, double au_to_forces, double in_veloc_to) :
+        en_from_au{in_en_from}, coords_to_au{in_coords_to}, veloc_to_au{in_veloc_to} {}
+
+    static Conversion from_elementary(double mass, double length, double time);
+
+    inline double energy_from_au(double en) { return en*en_from_au; }
+
+    template<typename itr1, typename itr2>
+    inline
+    void 
+    transform_coords_to_au(itr1 in_begin, itr1 in_end, itr2 result_begin) {
+        std::transform(in_begin, in_end, result_begin, [this](double val) -> double {return val*this->coords_to_au;});
+    };
+
+    template<typename itr1, typename itr2>
+    inline 
+    void 
+    transform_veloc_to_au(itr1 in_begin, itr1 in_end, itr2 result_begin) {
+        std::transform(in_begin, in_end, result_begin, [this](double val) -> double {return val*this->veloc_to_au;});
+    }
+
+    template<typename itr1, typename itr2>
+    inline 
+    void 
+    transform_au_to_forces(itr1 in_begin, itr1 in_end, itr2 result_begin) {
+        std::transform(in_begin, in_end, result_begin, [this](double val) -> double {return val*this->au_to_forces;});
+    }
+private:
+    // energies
+    double en_from_au;
+    // coordinates
+    double coords_to_au;
+    // velocities
+    double veloc_to_au;
+    // forces
+    double au_to_forces;
+};
+
+#endif
