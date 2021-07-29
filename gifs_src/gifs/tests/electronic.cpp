260ba339d5e6aa301d4b6fdc23338eaeda2a89e4
Vale Cofer-Shabica <valecs@sas.upenn.edu>
2021-07-29 00:29:55 -0400

    better test execution and examples

--- gifs_src/gifs/tests/electronic.cpp
+++ gifs_src/gifs/tests/electronic.cpp
@@ -3,30 +3,37 @@
 #include "electronic.hpp"
 #include <catch.hpp>
 #include <complex>
+#include "util.hpp"
 
-TEST_CASE( "2: A8", "[multi-file:2]"){
+double TrLg2(arma::mat A){
+  return std::real(arma::trace(arma::logmat(A).t()*arma::logmat(A)));
+}
+
+TEST_CASE( "Jacobi sweeps locate minimum for A", "[Electronic]"){
   arma::mat A8 = {{-0.6575,  0.3565, -0.6354, -0.1920},
                   {-0.1351, -0.6081, -0.4038,  0.6700},
                   {-0.0916, -0.6991, -0.0847, -0.7041},
                   {-0.7355, -0.1199,  0.6527,  0.1363}};
-  Electronic::phase_match(A8);
-  REQUIRE(std::real(arma::trace(arma::logmat(A8).t()*arma::logmat(A8))) < 8);
+
+  Electronic::phase_match(A8, false);
+  CHECK(TrLg2(A8) == Approx(6.8250));
+
+
+  Electronic::unitarize(A8);
+  REQUIRE((A8*A8.t() - arma::eye(arma::size(A8))).is_zero(1e-15));
 }
 
-// int main(void){
+TEST_CASE( "Jacobi sweeps locate minimum for B", "[Electronic]"){
+  arma::mat B8 = {{-0.5987, -0.1138,  0.5219,  0.5969},
+                  { 0.5288, -0.5520,  0.6321, -0.1274},
+                  { 0.5694,  0.1139, -0.2188,  0.7842},
+                  {-0.1942, -0.8182, -0.5294,  0.1121}};
 
-//   std::cout << "Tr[|log(A8)|^2]: " << arma::trace(arma::logmat(A8).t()*arma::logmat(A8)) << std::endl;
-//   std::cout << "|A8|: " << arma::det(A8) << std::endl;
-//   A8.print("A8");
-//   //FIXME: should implement phase_match w/out unitarize s.t. we can do matched = phase_match(unitarize(A));
-//   electronic::phase_match(A8);
-//   std::cout << "Tr[|log(A8)|^2]: " << arma::trace(arma::logmat(A8).t()*arma::logmat(A8)) << std::endl;
-//   std::cout << "|A8|: " << arma::det(A8) << std::endl;
-//   A8.print("A1?");
+  Electronic::phase_match(B8, false);
+  CHECK(TrLg2(B8) == Approx(7.7673));
 
-//   arma::mat I = A8 * A8.t();
-//   I.clean(1e-15);
-//   I.print("I?");
 
-//   return 0;
-// }
+  Electronic::unitarize(B8);
+  REQUIRE((B8*B8.t() - arma::eye(arma::size(B8))).is_zero(1e-15));
+}
+
